cf2d99bf911203578ae4be17cfef0bcd022fbefc
Raditz <radityohernanda@gmail.com>
2026-07-27 01:32:35 +0700

    chore: make title centered.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,15 +1,15 @@
 <script>
-  import CampaignForm from "./lib/components/CampaignForm.svelte";
+    import CampaignForm from "./lib/components/CampaignForm.svelte";
 </script>
 
 <div class="min-h-screen bg-base-200">
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="flex-1">
-      <span class="font-bold">Mini Email Campaign Sender</span>
+    <div class="navbar bg-base-100 shadow-sm">
+        <div class="navbar-center">
+            <span class="font-bold">Mini Email Campaign Sender</span>
+        </div>
     </div>
-  </div>
 
-  <main class="container mx-auto max-w-4xl px-4 py-8">
-    <CampaignForm />
-  </main>
+    <main class="container mx-auto max-w-4xl px-4 py-8">
+        <CampaignForm />
+    </main>
 </div>
